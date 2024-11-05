5bd50484bf9ab75f8a405a16927eb9ef5bf38f0a
“saikumar” <pawarsaikumar82@gmail.com>
2024-11-06 00:11:26 +0530

    Initial commit

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    int id;
+    String firstName;
+    String lastName;
+    
+    public Student(int id, String firstName, String lastName)
+    {
+        this.id = id;
+        this.firstName =firstName;
+        this.lastName = lastName
+    }
+}
