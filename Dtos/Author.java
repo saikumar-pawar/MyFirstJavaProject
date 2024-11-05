c88564c711bcda5e182da8089656165806549cae
“saikumar” <pawarsaikumar82@gmail.com>
2024-11-06 00:18:43 +0530

    Added Course

---
+++ Dtos/Author.java
@@ -0,0 +1,13 @@
+package Dtos;
+
+public class Author {
+    int id;
+    String firstName;
+    String lastName;
+
+    public Author(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ Dtos/Course.java
@@ -0,0 +1,11 @@
+package Dtos;
+
+public class Course {
+    String title;
+    String description;
+
+    public Course(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
